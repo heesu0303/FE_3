95895028143e18c945059b36c74c861e50bd9cea
Heesu <heesu0303@naver.com>
2022-11-22 18:59:54 +0900

    : 11.22 [Redux] Redux

---
+++ Redux/004/my-app/src/App.js
@@ -0,0 +1,11 @@
+import GoodsCounter from './modules/goodCounter.jsx'
+
+function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <GoodsCounter/>
+    </div>
+  );
+}
+export default App;

13cb3d8a88de25d8472ad4039f75c85ec4ecda20
Heesu <heesu0303@naver.com>
2022-11-24 16:14:21 +0900

    📝: 11.24 [JavaScript] SOP, CORS, 점진적 향상

---
+++ JavaScript/111_strict.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// str = 'hello strict';
+
+// var undefined = 'undefined';
+// var Infinity = 100;
+
+// Math.PI = 0;
+
+// const employee = {
+//     firstnae: 'heesu',
+//     lastname: 'jang'
+// }
+// console.log(employee.firstnae)
+// delete employee.firstnae;
+// console.log(employee.firstnae)
+
+// delete Object.prototype;
+// console.log(Object.prototype)
+
+function myFunc(a, a, b) {
+    console.log(a + a + b);
+}
+
+1 == 1;
+1 == "1";
+1 == 2;
+"" == false;
+[] == false; // 빈 배열은 심지어 truthy 입니다!
+null == undefined;

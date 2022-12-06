75477d114d4335001a3402f4d9afcddc152f0a07
Heesu <heesu0303@naver.com>
2022-12-06 17:50:58 +0900

    📝: 12.06 firebase

--- Firebase/my-diary/src/pages/home/Home.js
+++ Firebase/my-diary/src/pages/home/Home.js
@@ -1,5 +1,9 @@
-import styles from "./Home.module.css";
+import styles from './Home.module.css'
 
 export default function Home() {
-    return <div>HOME</div>;
+    return (
+        <div>
+            HOME
+        </div>
+    )
 }

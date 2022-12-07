1b98156268b5d83111fc8d4bcc3cf112ac713810
Heesu <heesu0303@naver.com>
2022-12-07 21:26:08 +0900

    📝: 12.07 firebase

---
+++ Firebase/my-diary/src/components/Nav.jsx
@@ -0,0 +1,35 @@
+import styles from "./Nav.module.css";
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
+
+export default function Nav() {
+    const { logout } = useLogout();
+    const { user } = useAuthContext();
+
+    return (
+        <nav className={styles.nav}>
+            <h1 className={styles.tit}>두근두근 비밀일기</h1>
+            <ul className={styles.list_nav}>
+                {!user && (
+                    <>
+                        <li>
+                            <Link to="/login">로그인</Link>
+                        </li>
+                        <li>
+                            <Link to="/signup">회원가입</Link>
+                        </li>
+                    </>
+                )}
+                {user && (
+                    <li>
+                        <strong>{user.displayName}님 안녕하세요👋</strong>
+                        <button type="button" onClick={logout}>
+                            로그아웃
+                        </button>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    );
+}
---
+++ Firebase/my-diary/src/pages/login/Login.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
+import styles from "./Login.module.css";
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { error, isPending, login } = useLogin();
+
+    const handleData = (event) => {
+        console.log(event.target.type);
+        if (event.target.type === "email") {
+            setEmail(event.target.value);
+        } else if (event.target.type === "password") {
+            setPassword(event.target.value);
+        }
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        login(email, password);
+        console.log(email, password);
+    };
+
+    return (
+        <form className={styles.login_form} onSubmit={handleSubmit}>
+            <fieldset>
+                <legend>로그인</legend>
+
+                <label htmlFor="myEmail">email : </label>
+                <input
+                    type="email"
+                    id="myEmail"
+                    required
+                    value={email}
+                    onChange={handleData}
+                />
+
+                <label htmlFor="myPassWord">password : </label>
+                <input
+                    type="password"
+                    id="myPassWord"
+                    required
+                    value={password}
+                    onChange={handleData}
+                />
+
+                {/* 로그인이 진행 전이라면 로그인 버튼을 노출하고 */}
+                {!isPending && (
+                    <button type="submit" className={styles.btn}>
+                        로그인
+                    </button>
+                )}
+                {/* 로그인이 진행 중이라면 로그인 버튼을 제거하고 정보를 표시합니다. */}
+                {isPending && <strong>로그인 진행중입니다.</strong>}
+                {error && <strong>{error}</strong>}
+            </fieldset>
+        </form>
+    );
+}
